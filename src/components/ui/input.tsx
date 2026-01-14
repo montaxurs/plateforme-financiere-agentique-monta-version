fc426520e209088e3478048e9e9c55d06c90ee88
montaxurs <monta7ssar@gmail.com>
2026-01-14 15:12:48 +0100

    Initial Commit!

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,6 +1,7 @@
+import { cn } from "@/src/lib/utils"
 import * as React from "react"
 
-import { cn } from "@/lib/utils"
+
 
 function Input({ className, type, ...props }: React.ComponentProps<"input">) {
   return (
